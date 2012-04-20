8ab3984d008e6354cc851e58ffd4070286ebd09a
kkolarov <k.p.kolarov@gmail.com>
2012-04-20 11:17:38 +0300

    test

--- scripts/cli-config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once '..\application\Bootstrap.php';
-
-$entityManager = Bootstrap::getInstance();
-
-$helperSet = new \Symfony\Component\Console\Helper\HelperSet(array(
-    'em' => new \Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper($entityManager)
-));
